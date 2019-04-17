ffc7c35ba82fb3256d9fcba37a0e8043f8ef0c77
Do Xuan Tho <dxtho@cmc.com.vn>
2019-04-17 16:25:02 +0700

    init voyager

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        TCG\Voyager\VoyagerServiceProvider::class,
 
         /*
          * Package Service Providers...
